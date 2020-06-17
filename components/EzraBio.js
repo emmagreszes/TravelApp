c20b5b5e71a2791c79c6c6219003d2d54886c35d
ezragershman <ezragersh@brandeis.edu>
2020-06-17 08:59:54 -0400

    Finished my Bio

--- components/EzraBio.js
+++ components/EzraBio.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Text, View, Button, Image } from 'react-native';
+import { Text, View, Button, Image, StyleSheet } from 'react-native';
 import { ezra } from '../assets/ezra.jpg';
 
 
@@ -8,9 +8,12 @@ export default function EzraBio({ route, navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Ezra's Bio</Text>
-      <img source=  {ezra} style= {{ width: 305, height: 159}}
-      alt = "ezra.jpg"/>
-      <Text>My name is Ezra Gershman. I am a CS and NEJS major. I am a rising senior
+      <Image
+        style= {{ width: 305, height: 300}}
+        source=  {require('../assets/ezra.jpg')}
+        alt = "ezra.jpg"
+      />
+      <Text style = {styles.text}>My name is Ezra Gershman. I am a CS and NEJS major. I am a rising senior
       at Brandeis. I am from Silver Spring, Maryland. In my free time, you can find me singing,
       napping, playing video games, or reading! </Text>
 
@@ -19,3 +22,10 @@ export default function EzraBio({ route, navigation }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+    textAlign: 'center',
+  },
+});
