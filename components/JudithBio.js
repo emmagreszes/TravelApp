8dadbffa750e5eecdb7c1d6878fae73e10019a3a
judithsherbin <jsherbin@brandeis.edu>
2020-06-16 23:10:20 -0400

    judth

--- components/JudithBio.js
+++ components/JudithBio.js
@@ -1,12 +1,13 @@
 import * as React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Image, Text, View, Button } from 'react-native';
+import { judithpic } from './assets/judith.jpeg'
 
 export default function JudithBio({ route, navigation }) {
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Judith's Bio</Text>
-      <Image source={require('./my-icon.png')} />;      
+      <Image source={judithpic} style= {{ width: 305, height: 159}} />;
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
