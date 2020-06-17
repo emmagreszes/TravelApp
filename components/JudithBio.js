a7a2edb9f548f12cf839aa453bae0cef14aa07f9
judithsherbin <jsherbin@brandeis.edu>
2020-06-16 21:56:57 -0400

    judith bio

--- components/JudithBio.js
+++ components/JudithBio.js
@@ -6,8 +6,7 @@ export default function JudithBio({ route, navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Judith's Bio</Text>
-
-      
+      <Image source={require('./my-icon.png')} />;      
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
