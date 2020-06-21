35c0db150d7a81c15a255bbecdbc691872170556
judithsherbin <jsherbin@brandeis.edu>
2020-06-21 17:41:22 -0400

    pizza

--- components/Wishlist.js
+++ components/Wishlist.js
@@ -1,14 +1,25 @@
-import * as React from 'react';
-import { Text, View, Button, Image, StyleSheet } from 'react-native';
+import React, { Component, useState } from 'react';
+import { Text, View, TextInput, Button, Image, StyleSheet } from 'react-native';
 import LocationList from '../containers/LocationList'
+import useStickyState from '../useStickyState';
 
 export default function Wishlist({ route, navigation }) {
-
+  const [text, setText] = useState('');
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 
       <LocationList/>
 
+
+      <TextInput
+        style={{height: 40}}
+        placeholder="Type here to translate!"
+        onChangeText={text => setText(text)}
+        defaultValue={text}
+      />
+      <Text style={{padding: 10, fontSize: 42}}>
+        {text.split(' ').map((word) => word && '🍕').join(' ')}
+      </Text>
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
