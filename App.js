c45f610dd17e8213a5e56d6dbc9d474e03d287a1
ezragershman <ezragersh@brandeis.edu>
2020-06-28 14:14:14 -0400

    ashbk

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, ImageBackground } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import About from "./components/About"
@@ -9,21 +9,16 @@ import JudithBio from "./components/JudithBio"
 import Wishlist from "./components/Wishlist"
 import Explore from "./components/Explore"
 import MyLocations from "./components/MyLocations"
-import Friends from "./components/Friends"
 import WishlistForm from "./components/forms/WishlistForm"
+
 // import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 // import Icon from "react-native-vector-icons/FontAwesome";
 
-const bios = [
-  {name:"Ezra",age:21, bio:"/.."},
-  {name:"Emma",age:22, bio:"h"},
-  {name:"Judith",age:22,bio:"wbhds"},
-]
 
 function HomeScreen({ navigation }) {
   return (
+
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', padding:'200'}}>
-      <Text style = {styles.text}>Home Screen</Text>
       <TouchableOpacity style = {styles.button}>
       <Button
         title="About"
@@ -46,11 +41,7 @@ function HomeScreen({ navigation }) {
         color="#57A892"
         onPress={() => {navigation.navigate("Explore")}}
       />
-      <Button
-        title="Friends"
-        color="#DB9B8F"
-        onPress={() => {navigation.navigate("Friends")}}
-      />
+
       </TouchableOpacity>
     </View>
   );
@@ -64,23 +55,15 @@ const styles = StyleSheet.create({
   button: {
     height: 200,
     width: 800,
-  }
+  },
+  backButton: {
+    height: 44,
+    width: 44,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
 
-function DetailsScreen({ route, navigation }) {
-  /* 2. Get the param */
-  const { name } = route.params;
-  const { age } = route.params;
-  return (
-    <View >
-      <Text>Details Screen</Text>
-      <Text>itemId: {JSON.stringify(name)}</Text>
-      <Text>otherParam: {JSON.stringify(age)}</Text>
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-    </View>
-  );
-}
 
 const Stack = createStackNavigator();
 
@@ -96,73 +79,9 @@ export default function App() {
         <Stack.Screen name="Wishlist" component={Wishlist}/>
         <Stack.Screen name="MyLocations" component={MyLocations}/>
         <Stack.Screen name="Explore" component={Explore}/>
-        <Stack.Screen name="Friends" component={Friends}/>
         <Stack.Screen name="WishlistForm" component={WishlistForm}/>
 
       </Stack.Navigator>
     </NavigationContainer>
   );
-  const styles = StyleSheet.create({
-   backButton: {
-     height: 44,
-     width: 44,
-     justifyContent: 'center',
-     alignItems: 'center',
-   },
-  });
 }
-
-
-
-// const bottomTabNavigator = createBottomTabNavigator(
-//   {
-//     Home: {
-//       screen: Home,
-//       navigationOptions: {
-//         tabBarIcon: ({ tintColor }) => (
-//           <Icon name="home" size={25} color={tintColor} />
-//         )
-//       }
-//     },
-//     Wishlist: {
-//       screen: Wishlist,
-//       navigationOptions: {
-//         tabBarIcon: ({ tintColor }) => (
-//           <Icon name="comments" size={25} color={tintColor} />
-//         )
-//       }
-//     },
-//     Explore: {
-//       screen: Explore,
-//       navigationOptions: {
-//         tabBarIcon: ({ tintColor }) => (
-//           <Icon name="search" size={25} color={tintColor} />
-//         )
-//       }
-//     },
-//     Friends: {
-//       screen: Friends,
-//       navigationOptions: {
-//         tabBarIcon: ({ tintColor }) => (
-//           <Icon name="search" size={25} color={tintColor} />
-//         )
-//       }
-//     },
-//     MyLocations: {
-//       screen: MyLocations,
-//       navigationOptions: {
-//         tabBarIcon: ({ tintColor }) => (
-//           <Icon name="user" size={25} color={tintColor} />
-//         )
-//       }
-//     },
-//   },
-//   {
-//     initialRouteName: 'Home',
-//     tabBarOptions: {
-//       activeTintColor: '#eb6e3d'
-//     }
-//   }
-// );
-//
-// const AppConta
